96470cc8304b6b2d266348c6f016ca2aa929786d
kamalkavin96 <kamalkavin68@gmail.com>
2025-09-23 10:38:31 +0530

    initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/main/java/com/farmedge/farmedge/FarmedgeApplication.java
@@ -0,0 +1,13 @@
+package com.farmedge.farmedge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmedgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmedgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmedge/farmedge/FarmedgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmedge.farmedge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmedgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
